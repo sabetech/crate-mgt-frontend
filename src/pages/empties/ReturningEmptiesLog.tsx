b95f67c1a69fb52dcc19130931d55fbc9fcc1309
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2023-05-19 06:00:20 +0000

    statistics

--- src/pages/empties/ReturningEmptiesLog.tsx
+++ src/pages/empties/ReturningEmptiesLog.tsx
@@ -6,7 +6,8 @@ import { useQuery } from '@tanstack/react-query';
 import { getEmptiesReturnedLog } from '../../services/EmptiesAPI';
 import type { ColumnsType } from 'antd/es/table';
 import { Card, Table } from 'antd';
-
+import { DatePicker, Row, Col, Statistic } from 'antd';
+const { RangePicker } = DatePicker;
 const ReturningEmptiesLog: React.FC = () => {
 
     //use react query to fetch data from server
@@ -16,6 +17,7 @@ const ReturningEmptiesLog: React.FC = () => {
     );
 
     const [emptiesReturnedLog, setEmptiesReturnedLog] = React.useState<IEmptyReturnedLog[] | undefined>(undefined);
+    const [dateRange, setDateRange] = React.useState<string[] | undefined>(undefined);
 
     useEffect(() => {
         if (data) {
@@ -26,6 +28,31 @@ const ReturningEmptiesLog: React.FC = () => {
             );
         }
     },[data]);
+
+    useEffect(() => {
+
+        if (dateRange) {
+            console.log(dateRange);
+            let [start, end] = dateRange;
+            let filteredData = data?.data?.filter((item) => {
+                let date = new Date(item.date);
+                let startDate = new Date(start);
+                let endDate = new Date(end);
+                return date >= startDate && date <= endDate;
+            });
+            setEmptiesReturnedLog(filteredData?.map((item) => ({
+                    ...item,
+                    key: item.id})
+                )
+            );
+        }
+
+    }, [dateRange])
+
+    const dateRangeOnChange = (date: any, dateString: string[]) => {
+        console.log(date, dateString);
+        setDateRange(dateString);
+    }
 
     const columns: ColumnsType<IEmptyReturnedLog> = [
         { title: 'Date', dataIndex: 'date', key: 'date' },
@@ -43,9 +70,29 @@ const ReturningEmptiesLog: React.FC = () => {
 
     return (
         <>
-            <Card>
-
-            </Card>
+            Select a date Range <RangePicker onChange={dateRangeOnChange}/>
+            <Row gutter={16}>
+                <Col span={12}>
+                    <Card bordered={true}>
+                        <Statistic
+                            title="Quantity Returned"
+                            value={emptiesReturnedLog ? emptiesReturnedLog.reduce((acc: number, item: IEmptyReturnedLog) => acc + item.quantity, 0) : 0}
+                            valueStyle={{ color: '#3f8600' }}
+                            suffix="empties"
+                        />
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card bordered={true}>
+                        <Statistic
+                            title="Quantity Balance"
+                            value={-123}
+                            valueStyle={{ color: '#ff0000' }}
+                            suffix="empties"
+                        />
+                    </Card>
+                </Col>
+            </Row>
             <TableEmptiesReturnedLog columns={columns} data={emptiesReturnedLog} />
         </>
     ); 
