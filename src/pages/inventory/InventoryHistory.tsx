da764ea8dba52c7135665b6bb2521c287e0d7cb2
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-04-18 12:59:08 +0000

    date picker in inventory historty

--- src/pages/inventory/InventoryHistory.tsx
+++ src/pages/inventory/InventoryHistory.tsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
-import { Typography } from "antd";
+import { DatePicker, Typography } from "antd";
+import dayjs from 'dayjs';
 
 const InventoryHistory = () => {
     const [inventoryHistory, setInventoryHistory] = useState([]);
+    const [date, setDate] = useState(dayjs().format('YYYY-MM-DD'))
 
-    
+
 
     return (<>
         <Typography.Title level={3}>Inventory History</Typography.Title>
+        <DatePicker 
+            value={dayjs(date)}
+            onChange={(date) => setDate(date.format('YYYY-MM-DD'))}
+        />
     </>)
 }
 
